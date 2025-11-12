247e7de7a1a146990b4a6aba43ceea72f5885d5e
Chrysr17 <chrisrasaro@gmail.com>
2025-11-12 17:04:22 -0500

    Categoria

---
+++ auto-service/src/main/java/com/example/autofinder/model/Categoria.java
@@ -0,0 +1,24 @@
+package com.example.autofinder.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+@Table(name = "categoria")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String descripcion;
+
+}
