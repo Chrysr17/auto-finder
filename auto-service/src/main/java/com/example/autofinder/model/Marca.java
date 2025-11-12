7072d3970be94e64813fc278d95cb84facb5b828
Chrysr17 <chrisrasaro@gmail.com>
2025-11-12 17:02:19 -0500

    Marca

---
+++ auto-service/src/main/java/com/example/autofinder/model/Marca.java
@@ -0,0 +1,24 @@
+package com.example.autofinder.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "marca")
+public class Marca {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre", nullable = false, unique = true)
+    private String nombre;
+
+}
