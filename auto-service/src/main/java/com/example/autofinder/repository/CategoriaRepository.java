2cb97d462ea712dfde9f3bcb26f84b2e0056e50f
Chrysr17 <chrisrasaro@gmail.com>
2025-11-13 18:40:57 -0500

    CategoriaRepository

--- auto-service/src/main/java/com/example/autofinder/repository/CategoriaRepository.java
+++ auto-service/src/main/java/com/example/autofinder/repository/CategoriaRepository.java
@@ -1,4 +1,9 @@
 package com.example.autofinder.repository;
 
-public interface CategoriaRepository {
+import com.example.autofinder.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 }
