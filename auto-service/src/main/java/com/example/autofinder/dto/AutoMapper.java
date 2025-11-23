639fc4093e569638b1d25e969db5ee4fa4c1da51
Chrysr17 <chrisrasaro@gmail.com>
2025-11-22 21:28:33 -0500

    mapper

--- auto-service/src/main/java/com/example/autofinder/dto/AutoMapper.java
+++ auto-service/src/main/java/com/example/autofinder/dto/AutoMapper.java
@@ -1,4 +1,26 @@
 package com.example.autofinder.dto;
 
-public class AutoMapper {
+import com.example.autofinder.model.Auto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface AutoMapper {
+    AutoMapper INSTANCE = Mappers.getMapper(AutoMapper.class);
+
+    // ENTITY → DTO
+    @Mapping(source = "marca.id", target = "marcaId")
+    @Mapping(source = "marca.nombre", target = "marcaNombre")
+    @Mapping(source = "modelo.id", target = "modeloId")
+    @Mapping(source = "modelo.nombre", target = "modeloNombre")
+    @Mapping(source = "categoria.id", target = "categoriaId")
+    @Mapping(source = "categoria.nombre", target = "categoriaNombre")
+    AutoDto toDTO(Auto auto);
+
+    // DTO → ENTITY (solo para registrar o actualizar)
+    @Mapping(target = "marca.id", source = "marcaId")
+    @Mapping(target = "modelo.id", source = "modeloId")
+    @Mapping(target = "categoria.id", source = "categoriaId")
+    Auto toEntity(AutoDto autoDTO);
 }
