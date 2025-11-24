40e59c61d23d4a2f77c68d343cf4b60e31066f97
Chrysr17 <chrisrasaro@gmail.com>
2025-11-24 10:17:40 -0500

    AutoRepository

--- auto-service/src/main/java/com/example/autofinder/repository/AutoRepositoy.java
+++ auto-service/src/main/java/com/example/autofinder/repository/AutoRepositoy.java
@@ -4,6 +4,10 @@ import com.example.autofinder.model.Auto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AutoRepositoy extends JpaRepository<Auto, Long> {
+    List<Auto> findByMarcaNombre(String marca);
+    List<Auto> findByCategoriaNombre(String categoria);
 }
