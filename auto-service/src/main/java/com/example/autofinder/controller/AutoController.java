e477584a97d775b14fcbda9474b0c192c9db9a16
Chrysr17 <chrisrasaro@gmail.com>
2025-11-24 10:37:20 -0500

    AutoController - listar

---
+++ auto-service/src/main/java/com/example/autofinder/controller/AutoController.java
@@ -0,0 +1,28 @@
+package com.example.autofinder.controller;
+
+import com.example.autofinder.dto.AutoDTO;
+import com.example.autofinder.service.AutoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/autos")
+public class AutoController {
+
+    private final AutoService autoService;
+
+    public AutoController(AutoService autoService) {
+        this.autoService = autoService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AutoDTO>> listar(){
+        List<AutoDTO> autos = autoService.listarTodos();
+        return ResponseEntity.ok(autos);
+    }
+
+}
