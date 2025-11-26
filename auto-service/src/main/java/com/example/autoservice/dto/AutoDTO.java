4c0bf50a1ad5dace208384e4c9a4ce7fcf9e074a
Chrysr17 <chrisrasaro@gmail.com>
2025-11-25 19:14:20 -0500

    autoDTO

--- auto-service/src/main/java/com/example/autoservice/dto/AutoDTO.java
+++ auto-service/src/main/java/com/example/autoservice/dto/AutoDTO.java
@@ -1,9 +1,13 @@
 package com.example.autoservice.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class AutoDTO {
     private Long id;
