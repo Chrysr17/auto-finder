e5d0c07c413fc584928a812c5b1370dc8813b2d7
Chrysr17 <chrisrasaro@gmail.com>
2025-11-27 15:25:36 -0500

    autoService

--- auto-service/src/main/java/com/example/autoservice/service/impl/AutoServiceImpl.java
+++ auto-service/src/main/java/com/example/autoservice/service/impl/AutoServiceImpl.java
@@ -1,9 +1,10 @@
 package com.example.autoservice.service.impl;
 
 import com.example.autoservice.dto.AutoDTO;
-import com.example.autoservice.mapper.AutoMapper;
-import com.example.autoservice.model.Auto;
 import com.example.autoservice.repository.AutoRepositoy;
+import com.example.autoservice.repository.CategoriaRepository;
+import com.example.autoservice.repository.MarcaRepository;
+import com.example.autoservice.repository.ModeloRepository;
 import com.example.autoservice.service.AutoService;
 import org.springframework.stereotype.Service;
 
@@ -13,67 +14,50 @@ import java.util.List;
 public class AutoServiceImpl implements AutoService {
 
     private final AutoRepositoy autoRepositoy;
-    private final AutoMapper autoMapper;
+    private final CategoriaRepository categoriaRepository;
+    private final MarcaRepository marcaRepository;
+    private final ModeloRepository modeloRepository;
 
-    public AutoServiceImpl(AutoRepositoy autoRepositoy, AutoMapper autoMapper) {
+    public AutoServiceImpl(AutoRepositoy autoRepositoy, CategoriaRepository categoriaRepository, MarcaRepository marcaRepository, ModeloRepository modeloRepository) {
         this.autoRepositoy = autoRepositoy;
-        this.autoMapper = autoMapper;
+        this.categoriaRepository = categoriaRepository;
+        this.marcaRepository = marcaRepository;
+        this.modeloRepository = modeloRepository;
     }
+
 
     @Override
     public List<AutoDTO> listarTodos() {
-        return autoRepositoy.findAll()
-                .stream()
-                .map(autoMapper::toDTO)
-                .toList();
+        return List.of();
     }
 
     @Override
     public AutoDTO buscarPorId(Long id) {
-        return autoRepositoy.findById(id)
-                .map(autoMapper::toDTO)
-                .orElse(null);
+        return null;
     }
 
     @Override
     public AutoDTO registrar(AutoDTO autoDTO) {
-        Auto auto = autoMapper.toEntity(autoDTO);
-        Auto guardado = autoRepositoy.save(auto);
-        return autoMapper.toDTO(guardado);
+        return null;
     }
 
     @Override
     public AutoDTO actualizar(Long id, AutoDTO autoDTO) {
-
-        Auto auto = autoRepositoy.findById(id)
-                .orElseThrow(() -> new RuntimeException("Auto no encontrado"));
-
-        Auto actualizado = autoMapper.toEntity(autoDTO);
-        actualizado.setId(id);
-
-        Auto guardado = autoRepositoy.save(actualizado);
-
-        return autoMapper.toDTO(guardado);
+        return null;
     }
 
     @Override
     public void eliminar(Long id) {
-        autoRepositoy.deleteById(id);
+
     }
 
     @Override
     public List<AutoDTO> buscarPorMarca(String marca) {
-        return autoRepositoy.findByMarcaNombre(marca)
-                .stream()
-                .map(autoMapper::toDTO)
-                .toList();
+        return List.of();
     }
 
     @Override
     public List<AutoDTO> buscarPorCategoria(String categoria) {
-        return autoRepositoy.findByCategoriaNombre(categoria)
-                .stream()
-                .map(autoMapper::toDTO)
-                .toList();
+        return List.of();
     }
 }
