551fd476c806d9d0521a70b3ad01ea05f216ba18
Chrysr17 <chrisrasaro@gmail.com>
2025-12-04 00:26:25 -0500

    SecurityConfig

--- auto-service/src/main/java/com/example/autoservice/security/SecurityConfig.java
+++ auto-service/src/main/java/com/example/autoservice/security/SecurityConfig.java
@@ -1,4 +1,13 @@
 package com.example.autoservice.security;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SecurityConfig {
+
+    private final JwtUtil jwtUtil;
+
+    public SecurityConfig(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
 }
