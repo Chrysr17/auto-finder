32ae1305e9fdd65e978343a0f0a8d87bcbddd55c
Chrysr17 <chrisrasaro@gmail.com>
2025-12-06 20:32:20 -0500

    AutoClient

--- comparador-service/src/main/java/com/example/comparador_service/client/AutoClient.java
+++ comparador-service/src/main/java/com/example/comparador_service/client/AutoClient.java
@@ -2,10 +2,13 @@ package com.example.comparador_service.client;
 
 import com.example.comparador_service.dto.AutoDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "auto-service", url = "http://localhost:9031")
 public interface AutoClient {
 
+    @GetMapping
     AutoDTO obtenerAuto(@PathVariable Long id);
+
 }
