7c586343e4c145833f772f24ada769ca40065453
Chrysr17 <chrisrasaro@gmail.com>
2025-12-06 20:12:46 -0500

    Comparador-Service

---
+++ comparador-service/src/main/java/com/example/comparador_service/ComparadorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.comparador_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComparadorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComparadorServiceApplication.class, args);
+	}
+
+}
---
+++ comparador-service/src/test/java/com/example/comparador_service/ComparadorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.comparador_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComparadorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
