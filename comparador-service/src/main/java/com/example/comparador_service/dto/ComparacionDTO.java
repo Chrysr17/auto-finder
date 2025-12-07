e0825a63628be7d221c319abb4781d69801c7ca4
Chrysr17 <chrisrasaro@gmail.com>
2025-12-06 20:27:00 -0500

    ComparacionDTO

---
+++ comparador-service/src/main/java/com/example/comparador_service/dto/ComparacionDTO.java
@@ -0,0 +1,19 @@
+package com.example.comparador_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ComparacionDTO {
+
+    private List<AutoDTO> autosComparados;
+
+    private String criterio;
+}
