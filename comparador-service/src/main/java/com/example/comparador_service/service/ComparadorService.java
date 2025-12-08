498393fde2a7775bb985945917337b4020fefe0b
Chrysr17 <chrisrasaro@gmail.com>
2025-12-08 17:07:02 -0500

    ComparadorService

--- comparador-service/src/main/java/com/example/comparador_service/service/ComparadorService.java
+++ comparador-service/src/main/java/com/example/comparador_service/service/ComparadorService.java
@@ -1,4 +1,9 @@
 package com.example.comparador_service.service;
 
+import com.example.comparador_service.dto.ComparacionDTO;
+
+import java.util.List;
+
 public interface ComparadorService {
+    ComparacionDTO compararAutos(List<Long> ids, String criterio);
 }
--- comparador-service/src/main/java/com/example/comparador_service/service/impl/ComparadorServiceImpl.java
+++ comparador-service/src/main/java/com/example/comparador_service/service/impl/ComparadorServiceImpl.java
@@ -1,4 +1,21 @@
 package com.example.comparador_service.service.impl;
 
-public class ComparadorServiceImpl {
+import com.example.comparador_service.client.AutoClient;
+import com.example.comparador_service.dto.ComparacionDTO;
+import com.example.comparador_service.service.ComparadorService;
+
+import java.util.List;
+
+public class ComparadorServiceImpl implements ComparadorService {
+
+    private final AutoClient autoClient;
+
+    public ComparadorServiceImpl(AutoClient autoClient) {
+        this.autoClient = autoClient;
+    }
+
+    @Override
+    public ComparacionDTO compararAutos(List<Long> ids, String criterio) {
+        return null;
+    }
 }
