0cc7003ad1431e560ca5ef69a0dc5f2f974a8ab0
Chrysr17 <chrisrasaro@gmail.com>
2025-12-08 17:13:58 -0500

    Properties

--- comparador-service/src/main/java/com/example/comparador_service/controller/ComparadorController.java
+++ comparador-service/src/main/java/com/example/comparador_service/controller/ComparadorController.java
@@ -1,4 +1,19 @@
 package com.example.comparador_service.controller;
 
+import com.example.comparador_service.service.ComparadorService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/comparar")
+@CrossOrigin("*")
 public class ComparadorController {
+
+    private final ComparadorService comparadorService;
+
+    public ComparadorController(ComparadorService comparadorService) {
+        this.comparadorService = comparadorService;
+    }
+
 }
