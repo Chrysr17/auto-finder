e79e54f7306dbf22a17247fe3786274b4e19b77e
Chrysr17 <chrisrasaro@gmail.com>
2025-12-08 17:17:02 -0500

    ComparadorController

--- comparador-service/src/main/java/com/example/comparador_service/controller/ComparadorController.java
+++ comparador-service/src/main/java/com/example/comparador_service/controller/ComparadorController.java
@@ -1,9 +1,11 @@
 package com.example.comparador_service.controller;
 
+import com.example.comparador_service.dto.ComparacionDTO;
 import com.example.comparador_service.service.ComparadorService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/comparar")
@@ -14,6 +16,15 @@ public class ComparadorController {
 
     public ComparadorController(ComparadorService comparadorService) {
         this.comparadorService = comparadorService;
+    }
+
+    public ResponseEntity<ComparacionDTO> comprar(
+            @RequestParam(required = false, defaultValue = "general") String criterio,
+            @RequestBody List<Long> ids
+            ){
+        return ResponseEntity.ok(
+                comparadorService.compararAutos(ids, criterio)
+        );
     }
 
 }
