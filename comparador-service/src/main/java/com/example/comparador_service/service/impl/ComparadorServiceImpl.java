7fa2dbc6f1e0a5213b5c259b7f4fc0163d9c504e
Chrysr17 <chrisrasaro@gmail.com>
2025-12-08 17:10:06 -0500

    ComparadorServiceImpl

--- comparador-service/src/main/java/com/example/comparador_service/service/impl/ComparadorServiceImpl.java
+++ comparador-service/src/main/java/com/example/comparador_service/service/impl/ComparadorServiceImpl.java
@@ -1,11 +1,15 @@
 package com.example.comparador_service.service.impl;
 
 import com.example.comparador_service.client.AutoClient;
+import com.example.comparador_service.dto.AutoDTO;
 import com.example.comparador_service.dto.ComparacionDTO;
 import com.example.comparador_service.service.ComparadorService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
 public class ComparadorServiceImpl implements ComparadorService {
 
     private final AutoClient autoClient;
@@ -16,6 +20,14 @@ public class ComparadorServiceImpl implements ComparadorService {
 
     @Override
     public ComparacionDTO compararAutos(List<Long> ids, String criterio) {
-        return null;
+
+        List<AutoDTO> autos = ids.stream()
+                .map(autoClient::obtenerAuto)
+                .collect(Collectors.toList());
+
+        return ComparacionDTO.builder()
+                .autosComparados(autos)
+                .criterio(criterio)
+                .build();
     }
 }
