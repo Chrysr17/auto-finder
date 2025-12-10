8240147ea0b745ed4667e2c3d520a2873146e5b3
Chrysr17 <chrisrasaro@gmail.com>
2025-12-10 11:24:32 -0500

    SecurityConfig

--- comparador-service/src/main/java/com/example/comparador_service/security/SecurityConfig.java
+++ comparador-service/src/main/java/com/example/comparador_service/security/SecurityConfig.java
@@ -1,4 +1,20 @@
 package com.example.comparador_service.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SecurityConfig {
+
+    private final JwtUtil jwtUtil;
+
+    public SecurityConfig(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter(){
+        return new JwtAuthenticationFilter(jwtUtil);
+    }
+
 }
