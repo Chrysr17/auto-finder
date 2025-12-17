6bb46c404cf120d98ab16157bbb75faaa5c2e5ee
Chrysr17 <chrisrasaro@gmail.com>
2025-12-16 19:30:20 -0500

    MarcaService

--- auto-service/src/main/java/com/example/autoservice/service/MarcaService.java
+++ auto-service/src/main/java/com/example/autoservice/service/MarcaService.java
@@ -1,4 +1,11 @@
 package com.example.autoservice.service;
 
+import com.example.autoservice.dto.MarcaDTO;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface MarcaService {
+    List<MarcaDTO> listar();
+    Optional<MarcaDTO> buscarPorId(Long id);
 }
