4209e3531a76b569622c4c6b5566a5a029778fe7
Chrysr17 <chrisrasaro@gmail.com>
2025-12-16 19:38:45 -0500

    ModeloService

--- auto-service/src/main/java/com/example/autoservice/service/ModeloService.java
+++ auto-service/src/main/java/com/example/autoservice/service/ModeloService.java
@@ -3,7 +3,10 @@ package com.example.autoservice.service;
 import com.example.autoservice.dto.ModeloDTO;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ModeloService {
     List<ModeloDTO> listar();
+    Optional<ModeloDTO> buscarPorId(Long id);
+    ModeloDTO registrar(ModeloDTO modeloDTO);
 }
