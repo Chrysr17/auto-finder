e738a47a14791fc14e9bdb3266602a1ac4c6a515
Chrysr17 <chrisrasaro@gmail.com>
2025-12-16 19:43:22 -0500

    CategoriaService

--- auto-service/src/main/java/com/example/autoservice/service/impl/CategoriaServiceImpl.java
+++ auto-service/src/main/java/com/example/autoservice/service/impl/CategoriaServiceImpl.java
@@ -1,12 +1,22 @@
 package com.example.autoservice.service.impl;
 
 import com.example.autoservice.dto.CategoriaDTO;
+import com.example.autoservice.repository.CategoriaRepository;
 import com.example.autoservice.service.CategoriaService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class CategoriaServiceImpl implements CategoriaService {
+
+    private final CategoriaRepository categoriaRepository;
+
+    public CategoriaServiceImpl(CategoriaRepository categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
+    }
+
     @Override
     public List<CategoriaDTO> listar() {
         return List.of();
