cfeed7c56d52f224c3e520b8ff48ab98d1832441
Chrysr17 <chrisrasaro@gmail.com>
2025-12-17 21:54:41 -0500

    CategoriaController

--- auto-service/src/main/java/com/example/autoservice/controller/CategoriaController.java
+++ auto-service/src/main/java/com/example/autoservice/controller/CategoriaController.java
@@ -1,6 +1,5 @@
 package com.example.autoservice.controller;
 
-import com.example.autoservice.dto.AutoRequestDTO;
 import com.example.autoservice.dto.CategoriaDTO;
 import com.example.autoservice.service.CategoriaService;
 import org.springframework.http.ResponseEntity;
@@ -8,19 +7,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping()
 public class CategoriaController {
 
-    private final CategoriaService categoriaService;
+    private final CategoriaService categoriaRepository;
 
-    public CategoriaController(CategoriaService categoriaService) {
-        this.categoriaService = categoriaService;
+    public CategoriaController(CategoriaService categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
     }
 
     @GetMapping
-    public ResponseEntity<CategoriaDTO> categoria(CategoriaDTO dto) {
-        return null;
+    public ResponseEntity<List<CategoriaDTO>> categoria(CategoriaDTO dto) {
+        List<CategoriaDTO> categorias = categoriaRepository.listar();
+        return ResponseEntity.ok(categorias);
     }
 
 }
