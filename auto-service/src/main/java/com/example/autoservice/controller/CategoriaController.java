fc10fb4c65d62a4802794390051007d050cf071e
Chrysr17 <chrisrasaro@gmail.com>
2025-12-19 21:34:55 -0500

    CategoriaController

--- auto-service/src/main/java/com/example/autoservice/controller/CategoriaController.java
+++ auto-service/src/main/java/com/example/autoservice/controller/CategoriaController.java
@@ -4,6 +4,7 @@ import com.example.autoservice.dto.CategoriaDTO;
 import com.example.autoservice.service.CategoriaService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,16 +14,24 @@ import java.util.List;
 @RequestMapping("/api/categorias")
 public class CategoriaController {
 
-    private final CategoriaService categoriaRepository;
+    private final CategoriaService categoriaService;
 
-    public CategoriaController(CategoriaService categoriaRepository) {
-        this.categoriaRepository = categoriaRepository;
+    public CategoriaController(CategoriaService categoriaService) {
+        this.categoriaService = categoriaService;
     }
+
 
     @GetMapping
     public ResponseEntity<List<CategoriaDTO>> categoria(CategoriaDTO dto) {
-        List<CategoriaDTO> categorias = categoriaRepository.listar();
+        List<CategoriaDTO> categorias = categoriaService.listar();
         return ResponseEntity.ok(categorias);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoriaDTO> buscarPorId(@PathVariable Long id) {
+        return categoriaService.buscarPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
 }
