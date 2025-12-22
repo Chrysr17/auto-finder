9966b28631736d8093f463c33ced59506ae0185e
Chrysr17 <chrisrasaro@gmail.com>
2025-12-22 16:26:47 -0500

    AutoMapper

--- auto-service/src/main/java/com/example/autoservice/mapper/AutoMapper.java
+++ auto-service/src/main/java/com/example/autoservice/mapper/AutoMapper.java
@@ -4,11 +4,32 @@ import com.example.autoservice.dto.AutoRequestDTO;
 import com.example.autoservice.dto.AutoResponseDTO;
 import com.example.autoservice.model.Auto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AutoMapper {
 
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "color", target = "color")
+    @Mapping(source = "precio", target = "precio")
+    @Mapping(source = "anioFabricacion", target = "anioFabricacion")
+
+    @Mapping(source = "marca.id", target = "marcaId")
+    @Mapping(source = "marca.nombre", target = "marcaNombre")
+
+    @Mapping(source = "modelo.id", target = "modeloId")
+    @Mapping(source = "modelo.nombre", target = "modeloNombre")
+
+    @Mapping(source = "categoria.id", target = "categoriaId")
+    @Mapping(source = "categoria.nombre", target = "categoriaNombre")
     AutoResponseDTO toResponseDTO(Auto auto);
 
+    @Mapping(source = "color", target = "color")
+    @Mapping(source = "precio", target = "precio")
+    @Mapping(source = "anioFabricacion", target = "anioFabricacion")
+
+    @Mapping(source = "marcaId", target = "marca.id")
+    @Mapping(source = "modeloId", target = "modelo.id")
+    @Mapping(source = "categoriaId", target = "categoria.id")
     Auto toEntity(AutoRequestDTO dto);
 }
