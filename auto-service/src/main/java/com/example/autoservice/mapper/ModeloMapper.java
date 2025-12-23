aed9d4872da1c3b1560dbf7c334701e69764ddf8
Chrysr17 <chrisrasaro@gmail.com>
2025-12-23 00:11:38 -0500

    modeloMapper

--- auto-service/src/main/java/com/example/autoservice/mapper/ModeloMapper.java
+++ auto-service/src/main/java/com/example/autoservice/mapper/ModeloMapper.java
@@ -3,10 +3,13 @@ package com.example.autoservice.mapper;
 import com.example.autoservice.dto.ModeloResponseDTO;
 import com.example.autoservice.model.Modelo;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ModeloMapper {
 
+    @Mapping(source = "marca.id", target = "marcaId")
+    @Mapping(source = "marca.nombre", target = "marcaNombre")
     ModeloResponseDTO toDTO(Modelo modelo);
 
     Modelo toEntity(ModeloResponseDTO dto);
