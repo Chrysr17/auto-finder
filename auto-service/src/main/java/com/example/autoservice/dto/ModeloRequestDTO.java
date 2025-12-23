112b54a5c7bfe6bff3eaae19ac31bdd39cb19b93
Chrysr17 <chrisrasaro@gmail.com>
2025-12-23 00:08:35 -0500

    modeloDTO

---
+++ auto-service/src/main/java/com/example/autoservice/dto/ModeloRequestDTO.java
@@ -0,0 +1,16 @@
+package com.example.autoservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ModeloRequestDTO {
+
+    private String nombre;
+    private Long marcaId;
+}
