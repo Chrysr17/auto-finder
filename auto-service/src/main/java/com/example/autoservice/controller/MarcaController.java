43cbb3d4addf630743d2dfe9615690d90f5ca548
Chrysr17 <chrisrasaro@gmail.com>
2025-12-23 20:33:21 -0500

    MarcaController

--- auto-service/src/main/java/com/example/autoservice/controller/MarcaController.java
+++ auto-service/src/main/java/com/example/autoservice/controller/MarcaController.java
@@ -1,4 +1,30 @@
 package com.example.autoservice.controller;
 
+import com.example.autoservice.dto.MarcaResponseDTO;
+import com.example.autoservice.service.MarcaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/marcas")
 public class MarcaController {
+
+    private final MarcaService marcaService;
+
+    public MarcaController(MarcaService marcaService) {
+        this.marcaService = marcaService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MarcaResponseDTO>> listar() {
+        List<MarcaResponseDTO> marcas = marcaService.listar();
+        return ResponseEntity.ok(marcas);
+    }
+
+
+
 }
