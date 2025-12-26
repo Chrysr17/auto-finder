6600d7b079a208c7194a336534e836839f1f2020
Chrysr17 <chrisrasaro@gmail.com>
2025-12-26 14:41:03 -0500

    MarcaController

--- auto-service/src/main/java/com/example/autoservice/controller/MarcaController.java
+++ auto-service/src/main/java/com/example/autoservice/controller/MarcaController.java
@@ -4,6 +4,7 @@ import com.example.autoservice.dto.MarcaResponseDTO;
 import com.example.autoservice.service.MarcaService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +26,11 @@ public class MarcaController {
         return ResponseEntity.ok(marcas);
     }
 
-
+    @GetMapping("/{id}")
+    public ResponseEntity<MarcaResponseDTO> buscarPorId(@PathVariable Long id) {
+        return marcaService.buscarPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
 
 }
