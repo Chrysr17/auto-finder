813d40ebeaf5d9d86027852ec4bbc896104f60db
Chrysr17 <chrisrasaro@gmail.com>
2025-12-27 23:19:52 -0500

    ModeloController

--- auto-service/src/main/java/com/example/autoservice/controller/ModeloController.java
+++ auto-service/src/main/java/com/example/autoservice/controller/ModeloController.java
@@ -1,4 +1,27 @@
 package com.example.autoservice.controller;
 
+import com.example.autoservice.dto.ModeloResponseDTO;
+import com.example.autoservice.service.ModeloService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/modelos")
 public class ModeloController {
+
+    private final ModeloService modeloService;
+
+    public ModeloController(ModeloService modeloService) {
+        this.modeloService = modeloService;
+    }
+
+    @RequestMapping
+    private ResponseEntity<List<ModeloResponseDTO>> listar(){
+        List<ModeloResponseDTO> modelos = modeloService.listar();
+        return ResponseEntity.ok(modelos);
+    }
+
 }
