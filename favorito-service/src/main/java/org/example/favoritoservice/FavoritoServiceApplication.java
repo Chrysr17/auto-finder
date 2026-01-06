1af8875101f3e7d2f7890ddeb28ecc65562485a5
Chrysr17 <chrisrasaro@gmail.com>
2026-01-06 13:32:00 -0500

    Favorito-service

---
+++ favorito-service/src/main/java/org/example/favoritoservice/FavoritoServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.favoritoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoritoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FavoritoServiceApplication.class, args);
+    }
+
+}
---
+++ favorito-service/src/test/java/org/example/favoritoservice/FavoritoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.favoritoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavoritoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
