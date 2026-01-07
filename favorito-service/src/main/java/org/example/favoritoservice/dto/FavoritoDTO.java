0bfbc7b840cad20c544cd845d8f45dccce8ce3ea
Chrysr17 <chrisrasaro@gmail.com>
2026-01-06 21:45:20 -0500

    favoritoDTO

--- favorito-service/src/main/java/org/example/favoritoservice/dto/FavoritoDTO.java
+++ favorito-service/src/main/java/org/example/favoritoservice/dto/FavoritoDTO.java
@@ -1,4 +1,18 @@
 package org.example.favoritoservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class FavoritoDTO {
+    private Long id;
+    private Long autoId;
+    private LocalDateTime fechaCreacion;
 }
