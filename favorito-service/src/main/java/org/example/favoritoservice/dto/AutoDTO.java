d307776aa74ab069f9140e43b83998b779ac54cb
Chrysr17 <chrisrasaro@gmail.com>
2026-01-07 13:46:42 -0500

    autoDTO

--- favorito-service/src/main/java/org/example/favoritoservice/dto/AutoDTO.java
+++ favorito-service/src/main/java/org/example/favoritoservice/dto/AutoDTO.java
@@ -1,4 +1,13 @@
 package org.example.favoritoservice.dto;
 
 public class AutoDTO {
+
+    private Long id;
+    private String color;
+    private String marcaNombre;
+    private String modeloNombre;
+    private String categoriaNombre;
+    private Double precio;
+    private Integer anioFabricacion;
+
 }
