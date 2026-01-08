0f6e58849f83226a282fef880992d24690806b99
Chrysr17 <chrisrasaro@gmail.com>
2026-01-08 13:08:31 -0500

    AutoDTO

--- favorito-service/src/main/java/org/example/favoritoservice/dto/AutoDTO.java
+++ favorito-service/src/main/java/org/example/favoritoservice/dto/AutoDTO.java
@@ -1,5 +1,14 @@
 package org.example.favoritoservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AutoDTO {
 
     private Long id;
