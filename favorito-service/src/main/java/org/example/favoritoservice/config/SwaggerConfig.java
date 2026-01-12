3b3844830bb25991d2625d10dbdc4f9f1af4064c
Chrysr17 <chrisrasaro@gmail.com>
2026-01-12 13:23:03 -0500

    swaggerConfig

---
+++ favorito-service/src/main/java/org/example/favoritoservice/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package org.example.favoritoservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API - FavoritoService")
+                        .version("1.0")
+                        .description("Microservicio para guardar autos favoritos"));
+
+    }
+
+}
