21e2f4806d36608038be6657b23c31e3c8362108
Chrysr17 <chrisrasaro@gmail.com>
2026-01-15 20:08:07 -0500

    securityConfig

---
+++ gateway-service/src/main/java/com/example/gatewayservice/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.example.gatewayservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+                .authorizeExchange(ex -> ex
+                        .pathMatchers("/api/auth/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .build();
+    }
+}
