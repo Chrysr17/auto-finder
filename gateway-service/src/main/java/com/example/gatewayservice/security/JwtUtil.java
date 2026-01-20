ea6c608e5480e8008da1d7ea5b36926970992278
Chrysr17 <chrisrasaro@gmail.com>
2026-01-20 18:11:27 -0500

    jwtUtil

--- gateway-service/src/main/java/com/example/gatewayservice/security/JwtUtil.java
+++ gateway-service/src/main/java/com/example/gatewayservice/security/JwtUtil.java
@@ -16,7 +16,7 @@ public class JwtUtil {
 
     private final Key key;
 
-    public JwtUtil(@Value("${jwt.key}") String secret) {
+    public JwtUtil(@Value("${jwt.secret}") String secret) {
         this.key = Keys.hmacShaKeyFor(secret.getBytes());
     }
 
