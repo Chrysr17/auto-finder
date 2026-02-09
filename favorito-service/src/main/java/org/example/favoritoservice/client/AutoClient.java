3eeaeb3e0ff47a80dfc6acfc2cd98cd6045fa214
Chrysr17 <chrisrasaro@gmail.com>
2026-02-08 23:41:50 -0500

    favorito-service autoClient

--- favorito-service/src/main/java/org/example/favoritoservice/client/AutoClient.java
+++ favorito-service/src/main/java/org/example/favoritoservice/client/AutoClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "auto-service", url = "AUTO_SERVICE_URL")
+@FeignClient(name = "auto-service", url = "${AUTO_SERVICE_URL}")
 public interface AutoClient {
 
     @GetMapping("/api/autos/{id}")
