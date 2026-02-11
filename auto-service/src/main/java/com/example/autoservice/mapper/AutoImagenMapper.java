ba63e8741759f4231ce3184e27baa7d4ea03d01f
Chrysr17 <chrisrasaro@gmail.com>
2026-02-11 12:44:58 -0500

    auto-service autoImagenMapper

---
+++ auto-service/src/main/java/com/example/autoservice/mapper/AutoImagenMapper.java
@@ -0,0 +1,12 @@
+package com.example.autoservice.mapper;
+
+import com.example.autoservice.dto.AutoImagenRequestDTO;
+import com.example.autoservice.dto.AutoImagenResponseDTO;
+import com.example.autoservice.model.AutoImagen;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface AutoImagenMapper {
+    AutoImagenResponseDTO toResponseDTO(AutoImagen autoImagen);
+    AutoImagen toEntityDTO(AutoImagenRequestDTO dto);
+}
