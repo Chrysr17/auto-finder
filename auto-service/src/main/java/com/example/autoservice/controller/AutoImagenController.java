299f9ca85a2615b4d02601948be5b2329a427387
Chrysr17 <chrisrasaro@gmail.com>
2026-02-17 21:34:05 -0500

    auto-service autoImagenController listarImagenes

--- auto-service/src/main/java/com/example/autoservice/controller/AutoImagenController.java
+++ auto-service/src/main/java/com/example/autoservice/controller/AutoImagenController.java
@@ -1,8 +1,14 @@
 package com.example.autoservice.controller;
 
+import com.example.autoservice.dto.AutoImagenResponseDTO;
 import com.example.autoservice.service.AutoImagenService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/autos")
@@ -13,4 +19,10 @@ public class AutoImagenController {
     public AutoImagenController(AutoImagenService autoImagenService) {
         this.autoImagenService = autoImagenService;
     }
+
+    @GetMapping("/{autoId}/imagenes")
+    public ResponseEntity<List<AutoImagenResponseDTO>> listarImagenes(@PathVariable Long autoId){
+        return ResponseEntity.ok(autoImagenService.listarImagenes(autoId));
+    }
+
 }
