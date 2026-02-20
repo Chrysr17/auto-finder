a73f771060c5154aa22bee8227715b7962e28740
Chrysr17 <chrisrasaro@gmail.com>
2026-02-19 23:26:48 -0500

    auto-service autoMapper - toDetalleResponseDTO

--- auto-service/src/main/java/com/example/autoservice/mapper/AutoMapper.java
+++ auto-service/src/main/java/com/example/autoservice/mapper/AutoMapper.java
@@ -1,5 +1,6 @@
 package com.example.autoservice.mapper;
 
+import com.example.autoservice.dto.AutoDetalleResponseDTO;
 import com.example.autoservice.dto.AutoRequestDTO;
 import com.example.autoservice.dto.AutoResponseDTO;
 import com.example.autoservice.model.Auto;
@@ -24,5 +25,17 @@ public interface AutoMapper {
     @Mapping(source = "categoria.nombre", target = "categoriaNombre")
     AutoResponseDTO toResponseDTO(Auto auto);
 
+    @Mapping(source = "marca.id", target = "marcaId")
+    @Mapping(source = "marca.nombre", target = "marcaNombre")
+    @Mapping(source = "modelo.id", target = "modeloId")
+    @Mapping(source = "modelo.nombre", target = "modeloNombre")
+    @Mapping(source = "categoria.id", target = "categoriaId")
+    @Mapping(source = "categoria.nombre", target = "categoriaNombre")
+    @Mapping(source = "imagenes", target = "imagenes")
+    AutoDetalleResponseDTO toDetalleResponseDTO(Auto auto);
+
     Auto toEntity(AutoRequestDTO dto);
+
+
+
 }
