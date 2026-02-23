2acefaa0aae12bba9ac0e0bf7511d70f33d80da1
Chrysr17 <chrisrasaro@gmail.com>
2026-02-23 12:35:52 -0500

    auto-service autoRepository listarAutoConPortada

--- auto-service/src/main/java/com/example/autoservice/repository/AutoRepositoy.java
+++ auto-service/src/main/java/com/example/autoservice/repository/AutoRepositoy.java
@@ -1,7 +1,9 @@
 package com.example.autoservice.repository;
 
 import com.example.autoservice.model.Auto;
+import com.example.autoservice.projection.AutoListadoProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +12,29 @@ import java.util.List;
 public interface AutoRepositoy extends JpaRepository<Auto, Long> {
     List<Auto> findByMarcaNombre(String marca);
     List<Auto> findByCategoriaNombre(String categoria);
+
+    @Query("""
+        SELECT
+            a.id as id,
+            a.color as color,
+            a.precio as precio,
+            a.anioFabricacion as anioFabricacion,
+
+            m.id as marcaId,
+            m.nombre as marcaNombre,
+
+            mo.id as modeloId,
+            mo.nombre as modeloNombre,
+
+            c.id as categoriaId,
+            c.nombre as categoriaNombre,
+
+            ai.url as imagenPortadaUrl
+        FROM Auto a
+        JOIN a.marca m
+        JOIN a.modelo mo
+        JOIN a.categoria c
+        LEFT JOIN a.imagenes ai ON ai.orden = 1
+    """)
+    List<AutoListadoProjection> listarAutosConPortada();
 }
