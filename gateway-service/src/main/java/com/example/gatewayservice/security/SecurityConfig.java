30837031b61bf0e6b38272c6e3415f9c65c63661
Chrysr17 <chrisrasaro@gmail.com>
2026-03-06 23:49:35 -0500

    gateway-service SecurityConfig

--- gateway-service/src/main/java/com/example/gatewayservice/security/SecurityConfig.java
+++ gateway-service/src/main/java/com/example/gatewayservice/security/SecurityConfig.java
@@ -2,11 +2,22 @@ package com.example.gatewayservice.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
+
+    private final JwtAuthWebFilter jwtAuthWebFilter;
+
+    public SecurityConfig(JwtAuthWebFilter jwtAuthWebFilter) {
+        this.jwtAuthWebFilter = jwtAuthWebFilter;
+    }
+
+
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         return http
@@ -17,6 +28,7 @@ public class SecurityConfig {
                         .pathMatchers("/api/auth/**").permitAll()
                         .anyExchange().authenticated()
                 )
+                .addFilterAt(jwtAuthWebFilter, SecurityWebFiltersOrder.AUTHENTICATION)
                 .build();
     }
 }
