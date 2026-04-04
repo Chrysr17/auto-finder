3a032cf9a9a31e87ce51c69966da0f36ad126fb1
Chrysr17 <chrisrasaro@gmail.com>
2026-04-04 13:17:17 -0500

    auto-service service-registrar/actualizar - AutoCreateRequestDTO/AutoUpdateRequestDTO

--- auto-service/src/main/java/com/example/autoservice/service/AutoService.java
+++ auto-service/src/main/java/com/example/autoservice/service/AutoService.java
@@ -1,9 +1,10 @@
 package com.example.autoservice.service;
 
 import com.example.autoservice.dto.AutoBusquedaResponseDTO;
+import com.example.autoservice.dto.AutoCreateRequestDTO;
 import com.example.autoservice.dto.AutoFiltroRequestDTO;
-import com.example.autoservice.dto.AutoRequestDTO;
 import com.example.autoservice.dto.AutoResponseDTO;
+import com.example.autoservice.dto.AutoUpdateRequestDTO;
 
 import java.util.List;
 
@@ -11,8 +12,8 @@ public interface AutoService {
     List<AutoResponseDTO> listarTodos();
     AutoBusquedaResponseDTO buscarConFiltros(AutoFiltroRequestDTO filtro);
     AutoResponseDTO buscarPorId(Long id);
-    AutoResponseDTO registrar(AutoRequestDTO autoRequestDTO);
-    AutoResponseDTO actualizar(Long id, AutoRequestDTO autoRequestDTO);
+    AutoResponseDTO registrar(AutoCreateRequestDTO autoRequestDTO);
+    AutoResponseDTO actualizar(Long id, AutoUpdateRequestDTO autoRequestDTO);
     void eliminar(Long id);
     List<AutoResponseDTO> buscarPorMarca(String marca);
     List<AutoResponseDTO> buscarPorCategoria(String categoria);
