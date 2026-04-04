db6958502128c5d9d3abc0d16fa63c355a6bca09
Chrysr17 <chrisrasaro@gmail.com>
2026-04-04 13:13:53 -0500

    auto-service AutoCreateRequestDTO/AutoUpdateRequestDTO

---
+++ auto-service/src/main/java/com/example/autoservice/dto/AutoUpdateRequestDTO.java
@@ -0,0 +1,37 @@
+package com.example.autoservice.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AutoUpdateRequestDTO {
+
+    @NotBlank
+    private String color;
+
+    @NotNull
+    @Positive
+    private Double precio;
+
+    @NotNull
+    @Min(1886)
+    private Integer anioFabricacion;
+
+    @NotNull
+    private Long marcaId;
+
+    @NotNull
+    private Long modeloId;
+
+    @NotNull
+    private Long categoriaId;
+}
