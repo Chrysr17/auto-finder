48eed4e543674f217a0f5c8e3bc58a7d29a856ed
Chrysr17 <chrisrasaro@gmail.com>
2026-04-17 22:36:18 -0500

    comparador-service autoDTO V2

--- comparador-service/src/main/java/com/example/comparador_service/dto/AutoDTO.java
+++ comparador-service/src/main/java/com/example/comparador_service/dto/AutoDTO.java
@@ -15,7 +15,25 @@ public class AutoDTO {
 
     private String color;
     private Double precio;
+    private Double precioReferenciaActual;
+    private Double precioSalidaEstimado;
     private Integer anioFabricacion;
+    private String motor;
+    private Integer cilindradaCc;
+    private Integer caballosFuerza;
+    private Integer torqueNm;
+    private Double consumoCiudad;
+    private Double consumoCarretera;
+    private Integer velocidadMaxima;
+    private Double aceleracionCeroACien;
+    private String tipoCombustible;
+    private String transmision;
+    private String traccion;
+    private Integer pesoKg;
+    private Integer puertas;
+    private String moneda;
+    private String descripcionValor;
+    private String resumen;
 
     private Long marcaId;
     private String marcaNombre;
