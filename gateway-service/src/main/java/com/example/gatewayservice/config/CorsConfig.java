4eb6a0b63ea59c5d590ec95549b109f6ff0b7468
Chrysr17 <chrisrasaro@gmail.com>
2026-05-03 14:14:19 -0500

    corsConfig v2

--- gateway-service/src/main/java/com/example/gatewayservice/config/CorsConfig.java
+++ gateway-service/src/main/java/com/example/gatewayservice/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration cors = new CorsConfiguration();
         cors.setAllowedOrigins(List.of("http://localhost:3000"));
-        cors.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
+        cors.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
         cors.setAllowedHeaders(List.of("*"));
         cors.setAllowCredentials(true);
 
